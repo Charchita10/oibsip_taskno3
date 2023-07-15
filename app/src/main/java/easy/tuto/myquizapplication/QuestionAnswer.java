2e1cf948120991468760eda17ac1a393b8dcaedd
Charchita <charchitapanda20022@gmail.com>
2023-07-15 21:03:55 +0530

    first commit

---
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,27 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "India is a federal union comprising twenty-eight states and how many union territories?",
+            "Who is the CEO of Twitter?",
+            "What is the state flower of Haryana?",
+            "Who is the CEO of Amazon?"
+
+    };
+
+    public static String choices[][] = {
+            {"6","7","8","9"},
+            {"Steve Jobs","Bill Gates","Jeff Bezos","Elon Musk"},
+            {"Lotus","Sunflower","Rose","Lily"},
+            {"Steve Jobs","Bill Gates","Jeff Bezos","Elon Musk"}
+    };
+
+    public static String correctAnswers[] = {
+            "8",
+            "Elon Musk",
+            "Lotus",
+            "Jeff Bezos"
+    };
+
+}
